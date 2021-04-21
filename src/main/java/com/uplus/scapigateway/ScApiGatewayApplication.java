ecbe191cde1775d5de4d09f45c132de9869d14cc
seaking7 <seaking7@gmail.com>
2021-04-21 18:35:43 +0900

    Initial commit

---
+++ src/main/java/com/uplus/scapigateway/ScApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.uplus.scapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScApiGatewayApplication.class, args);
+	}
+
+}
