043504d96e4c622cb45f6a54ceedf491b09fa0ce
Kim Taekyung <seaking7@gmail.com>
2021-04-28 21:23:38 +0900

    rabbitMQ연동 등

--- src/main/java/com/uplus/scapigateway/ScApiGatewayApplication.java
+++ src/main/java/com/uplus/scapigateway/ScApiGatewayApplication.java
@@ -1,13 +1,21 @@
 package com.uplus.scapigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ScApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository(){
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
